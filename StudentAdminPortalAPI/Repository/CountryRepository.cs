8b7b29b1d1ddbd5e23fdcd40f531724e5941fc1b
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-08-31 11:19:03 +0500

    create country interface and repository class

---
+++ StudentAdminPortalAPI/Repository/CountryRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAdminPortalAPI.Data;
+using StudentAdminPortalAPI.Model;
+using StudentAdminPortalAPI.ViewModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortalAPI.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CountryRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            return await _applicationDbContext.Departments.AnyAsync(x => x.DepartmentId == id);
+        }
+
+        public async Task<List<Country>> GetAllCountryAsync()
+        {
+            return await _applicationDbContext.Departments
+               .ToListAsync();
+        }
+
+        public async Task<Country> GetCountryById(int id)
+        {
+            return await _applicationDbContext.Departments.Where(x => x.DepartmentId == id).
+                FirstOrDefaultAsync();
+        }
+
+        public Task<Country> InsertCountry(Country c)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
---
+++ StudentAdminPortalAPI/Repository/ICountryRepository.cs
@@ -0,0 +1,16 @@
+﻿using StudentAdminPortalAPI.Model;
+using StudentAdminPortalAPI.ViewModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortalAPI.Repository
+{
+    public interface ICountryRepository
+    {
+        Task<List<Country>> GetAllCountryAsync();
+        Task<Country> GetCountryById(int id);    
+        Task<bool> Exists(int id);        
+        Task<Country> InsertCountry(Country c);
+        
+    }
+}
